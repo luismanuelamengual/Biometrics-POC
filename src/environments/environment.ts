348ba2c5fef97420a2d0786f45355255c2dd4992
luis.amengual <luis.amengual@globant.com>
2020-03-15 18:08:55 -0300

    Ajustes para el nuevo código

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    biometricsUrl: 'https://localhost:8080',
+    biometricsUrl: '/biometrics_local',
     biometricsApiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhdXRoMCIsImNsaWVudCI6Ikdsb2JhbnQifQ.D1wrBW1Pe3CWO6JiLdQFOwmqD8Vlz_nRH4sX0KTth2U'
 };
